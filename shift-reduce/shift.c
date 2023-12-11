a7ef8848fc236c01f8575d1fa8a7ad3578da91c3
Melvin John <melvinjohn2001@gmail.com>
2023-12-11 23:25:23 +0530

    Update shift.c

--- shift-reduce/shift.c
+++ shift-reduce/shift.c
@@ -12,10 +12,9 @@ int main()
       c=strlen(a);
       strcpy(act,"SHIFT->");
       puts("stack \t input \t action");
-      for(k=0,i=0; j<c; k++,i++,j++)
-       {
-         if(a[j]=='i' && a[j+1]=='d')
-           {
+      
+      for(k=0,i=0; j<c; k++,i++,j++) {
+         if(a[j]=='i' && a[j+1]=='d') {
               stk[i]=a[j];
               stk[i+1]=a[j+1];
               stk[i+2]='\0';
@@ -24,8 +23,7 @@ int main()
               printf("\n$%s\t%s$\t%sid",stk,a,act);
               check();
            }
-         else
-           {
+         else {
               stk[i]=a[j];
               stk[i+1]='\0';
               a[j]=' ';
@@ -33,44 +31,35 @@ int main()
               check();
            }
        }
+   }
 
-   }
-void check()
-   {
-     strcpy(ac,"REDUCE TO E");
-     for(z=0; z<c; z++)
-       if(stk[z]=='i' && stk[z+1]=='d')
-         {
-           stk[z]='E';
-           stk[z+1]='\0';
-           printf("\n$%s\t%s$\t%s",stk,a,ac);
-           j++;
-         }
-     for(z=0; z<c; z++)
-       if(stk[z]=='E' && stk[z+1]=='+' && stk[z+2]=='E')
-         {
-           stk[z]='E';
-           stk[z+1]='\0';
-           stk[z+2]='\0';
-           printf("\n$%s\t%s$\t%s",stk,a,ac);
-           i=i-2;
-         }
-     for(z=0; z<c; z++)
-       if(stk[z]=='E' && stk[z+1]=='*' && stk[z+2]=='E')
-         {
-           stk[z]='E';
-           stk[z+1]='\0';
-           stk[z+1]='\0';
-           printf("\n$%s\t%s$\t%s",stk,a,ac);
-           i=i-2;
-         }
-     for(z=0; z<c; z++)
-       if(stk[z]=='(' && stk[z+1]=='E' && stk[z+2]==')')
-         {
-           stk[z]='E';
-           stk[z+1]='\0';
-           stk[z+1]='\0';
-           printf("\n$%s\t%s$\t%s",stk,a,ac);
-           i=i-2;
-         }
-   }
+void check() {
+    strcpy(ac, "REDUCE TO E");
+
+    for (z = 0; z < c; z++) {
+        if (stk[z] == 'i' && stk[z + 1] == 'd') {
+            stk[z] = 'E';
+            stk[z + 1] = '\0';
+            printf("\n$%s\t%s$\t%s", stk, a, ac);
+            j++;
+        } else if (stk[z] == 'E' && stk[z + 1] == '+' && stk[z + 2] == 'E') {
+            stk[z] = 'E';
+            stk[z + 1] = '\0';
+            stk[z + 2] = '\0';
+            printf("\n$%s\t%s$\t%s", stk, a, ac);
+            i = i - 2;
+        } else if (stk[z] == 'E' && stk[z + 1] == '*' && stk[z + 2] == 'E') {
+            stk[z] = 'E';
+            stk[z + 1] = '\0';
+            stk[z + 2] = '\0';
+            printf("\n$%s\t%s$\t%s", stk, a, ac);
+            i = i - 2;
+        } else if (stk[z] == '(' && stk[z + 1] == 'E' && stk[z + 2] == ')') {
+            stk[z] = 'E';
+            stk[z + 1] = '\0';
+            stk[z + 2] = '\0';
+            printf("\n$%s\t%s$\t%s", stk, a, ac);
+            i = i - 2;
+        }
+    }
+}
